2d8e2291e3996c3378614453c2a044cb1c46f358
odar <oswaldoodavidal@gmail.com>
2017-05-01 16:14:28 -0400

    Hover effect applied

--- javascript/script.js
+++ javascript/script.js
@@ -5,16 +5,12 @@ $(document).ready(function () {
     for (var i = 0; i < gridSize; i++) {
         for (var j = 0; j < gridSize; j++) {
             var $divCell = $("<div class='cell'></div>");
-            $divCell.css("display", "inline-block");
-            $divCell.width("40px");
-            $divCell.height("40px");
-            $divCell.css("margin", "16px");
-            $divCell.css("background-color", "blue");
             $divGrid.append($divCell);
         }
     }
 
-    $("#cell").mouseenter(function () {
-        $(this).toggleClass()
+    $(".cell").mouseenter(function () {
+        $(this).toggleClass("cell");
+        $(this).toggleClass("hoverCell");
     });
 });
