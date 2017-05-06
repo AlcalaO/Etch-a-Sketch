752f724ba22529b0a28a98ede601da0bd478dff2
odar <oswaldoodavidal@gmail.com>
2017-05-06 16:08:19 -0400

    Little changes

--- javascript/script.js
+++ javascript/script.js
@@ -1,16 +1,51 @@
 $(document).ready(function () {
-    var gridSize = 16;
+    var cells = 16;
     var $divGrid = $("#grid");
+    var $buttonRestart = $("#restartButton");
+    // $buttonRestart.bind("click", restart());
 
-    for (var i = 0; i < gridSize; i++) {
-        for (var j = 0; j < gridSize; j++) {
+    // createGrid(cells, $divGrid.width());
+    for (var i = 0; i < cells; i++) {
+        for (var j = 0; j < cells; j++) {
             var $divCell = $("<div class='cell'></div>");
             $divGrid.append($divCell);
         }
     }
 
-    $(".cell").mouseenter(function () {
-        $(this).toggleClass("cell");
+    $divCell.mouseenter(function () {
         $(this).toggleClass("hoverCell");
     });
+
+    $(".createdCell").bind('hover', changeClass($(this)));
+
+    funtcion changeClass(element) {
+        element.toggleClass("hoverCell");
+    }
+
+    $buttonRestart.click(function restart() {
+        $divGrid.empty();
+        cells = prompt("Enter a new size for the grid:");
+        createGrid(cells,calculateCellsSize(cells, $divGrid.width()));
+    });
+
+    function calculateCellsSize(cells, gridSize) {
+        var cellsSize = gridSize / cells;
+
+        return cellsSize;
+    }
+
+    function createGrid (cells, cellsSize) {
+        $divGrid.css("grid-template-columns", "repeat("+cells+", "+cellsSize+"px)");
+        var cellMargin = cellsSize * 0.15;
+        for (var i = 0; i < cells; i++) {
+            for (var j = 0; j < cells; j++) {
+                var $divCreatedCell = $("<div class='cell'></div>");
+                $divCell.width(cellsSize+"px");
+                $divCell.height(cellsSize+"px");
+                $divGrid.append($divCell);
+            }
+        }
+        $divGrid.css("display", "grid");
+    }
+
 });
